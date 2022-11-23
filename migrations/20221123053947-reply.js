ff0c7141ddd69ecb1e40eb4bbaaece0e3a04087c
prasath2201crayond <prasath2201crayond>
2022-11-23 13:53:46 +0530

    update

--- migrations/20221123053947-reply.js
+++ migrations/20221123053947-reply.js
@@ -1,22 +1,32 @@
 "use strict";
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable("replies", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      command_id: {
+        type: Sequelize.UUID,
+        references: { model: "commands", key: "id" },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      command: {
+        type: Sequelize.STRING,
+      },
+    });
   },
-
   async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable("replies");
   },
 };
